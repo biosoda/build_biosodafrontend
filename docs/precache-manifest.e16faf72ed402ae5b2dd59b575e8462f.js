8fc0c4bdd20712eac26be8a0d8d865d225d7f8fd
biosoda <47027626+biosoda@users.noreply.github.com>
2023-09-20 18:33:39 +0200

    Add bioquery compiled webapp

---
+++ docs/precache-manifest.e16faf72ed402ae5b2dd59b575e8462f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0de108b9a7331ef1dea06ce38e68ea9",
+    "url": "/build_biosodafrontend/static/media/chevron-right.a0de108b.svg"
+  },
+  {
+    "revision": "b0ca7d8035e50297ae530cab9f85b139",
+    "url": "/build_biosodafrontend/static/media/chevron-down.b0ca7d80.svg"
+  },
+  {
+    "revision": "5d19200f3c82c3310864",
+    "url": "/build_biosodafrontend/static/js/runtime~main.c56559fd.js"
+  },
+  {
+    "revision": "864eb4c67beea77f3784",
+    "url": "/build_biosodafrontend/static/js/main.34dcb94e.chunk.js"
+  },
+  {
+    "revision": "67cbdfcd6bbed65a8157",
+    "url": "/build_biosodafrontend/static/js/2.a31d7b76.chunk.js"
+  },
+  {
+    "revision": "864eb4c67beea77f3784",
+    "url": "/build_biosodafrontend/static/css/main.19427afd.chunk.css"
+  },
+  {
+    "revision": "67cbdfcd6bbed65a8157",
+    "url": "/build_biosodafrontend/static/css/2.514e7bce.chunk.css"
+  },
+  {
+    "revision": "dbadd6712e47a93d65082747e75eae7d",
+    "url": "/build_biosodafrontend/index.html"
+  }
+];
